daf1cddcdeae594d987a65d78ac51822988e2da6
Letizia <letizia.latini@studenti.unicam.it>
2025-05-16 09:57:32 +0200

    ho eliminato index.css perchè andava in confilitto con app.css

--- ricette-app/src/main.jsx
+++ ricette-app/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import './App.css';
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecipeDetails from "./RecipeDetails";
 import Registrazione from "./Registrazione"; 
